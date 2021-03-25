091de7f180adeeae918ee3f6988590a29850dbbc
cbalieiro <bmoliveira.camila@gmail.com>
2021-03-25 20:42:31 -0300

    configuraçoes de ambiente no index

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const routes = require('./server/routes/index')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const routes = require("./server/routes/index");
 
-app.use('/', routes);
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('*', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors());
+app.use(express.json());
+app.use("/", routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`Example app listening at http://localhost:${port}`);
+});
