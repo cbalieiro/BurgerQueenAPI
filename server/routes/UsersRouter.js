d2a6d1b18aa5b6bf99cdf5d7e6ef1cc430cbf899
cbalieiro <bmoliveira.camila@gmail.com>
2021-03-25 21:23:03 -0300

    controllers/ associando arquivos de rotas aos controllers e exemplos

--- server/routes/UsersRouter.js
+++ server/routes/UsersRouter.js
@@ -1,9 +1,8 @@
 const { Router } = require("express");
-const ExampleController = require("../controller/ExampleController");
+const ExampleController = require("../controller/users/ExampleController");
 
 const router = Router();
 
-// aqui vai as requisições
 router.get("/", ExampleController.getAllExamples);
 
 module.exports = router;
