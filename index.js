c981aa8d4cd6cc5fbdb920b2d815f0ce83b79dda
cbalieiro <bmoliveira.camila@gmail.com>
2021-03-27 16:39:57 -0300

    alterando porta para testes com heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const routes = require("./server/routes/index");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use("/", routes);
 // app.use((err, req, res, next) => {
