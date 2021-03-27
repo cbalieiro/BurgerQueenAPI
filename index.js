15b9fffd0e3acb12386910f52aeb80531676a113
cbalieiro <bmoliveira.camila@gmail.com>
2021-03-27 16:32:18 -0300

    alterando porta para testes com heroku

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
-app.use((err, req, res, next) => {
-  if (["production", "test", "development"].includes(process.env.NODE_ENV))
-    res.status(500).json({ error: "internal server error" });
-  else return next(err);
-});
+// app.use((err, req, res, next) => {
+//   // if (["production", "test", "development"].includes(process.env.NODE_ENV))
+//   //   res.status(500).json({ error: "internal server error" });
+//   // else 
+//   return next(err);
+// });
 
 app.get("/", (req, res) => {
   res.send("Welcome to the API developed by @cbalieiro!!!");
